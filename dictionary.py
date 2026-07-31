10034d765830cb7ea5a81706661dd06ad38016b5
vinaypannangi <vinaypannangi@gmail.com>
2026-07-31 16:48:50 +0530

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -2,22 +2,22 @@ d1={'Ap':'Amaravathi','Ap':'Guntur',2:4,8:64,2:16,'Nandyal':'Alagadda'}
 print(d1)
 print(type(d1))
 
-#pop
-d1.pop('Ap')
-print(d1)
-
-#popitem
-# d1.popitem()
+# #pop
+# d1.pop('Ap')
 # print(d1)
 
-#get
-print(d1.get('Ap'))
+# #popitem
+# # d1.popitem()
+# # print(d1)
 
-#keys
-print(d1.keys())
+# #get
+# print(d1.get('Ap'))
 
-#values
-print(d1.values())
+# #keys
+# print(d1.keys())
+
+# #values
+# print(d1.values())
 
 #for(int i=0;i<10;i++){
 #   logic 
@@ -26,3 +26,12 @@ print(d1.values())
 
 #for i in range(1,10,2)
 #  print('odd nos are=',i)
+
+for i in range(0,10,2):
+    print('even nos are=',i)
+
+d1.setdefault('vinay','Python developer')
+print(d1)    
+
+d1['vinay']='java'
+print(d1)
---
+++ list.py
@@ -0,0 +1,8 @@
+l1=[1,2,3]
+print(l1)
+l1.append(4)
+print(l1)
+l1.extend([5,6,7])
+print(l1)
+l1.insert(2,90)
+print(l1)
