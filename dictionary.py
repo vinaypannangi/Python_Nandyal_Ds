2204d7f3e3afacdc4e0de0ca80e3af388a7b43b8
vinaypannangi <vinaypannangi@gmail.com>
2026-07-31 16:26:46 +0530

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -7,5 +7,22 @@ d1.pop('Ap')
 print(d1)
 
 #popitem
-d1.popitem()
-print(d1)
+# d1.popitem()
+# print(d1)
+
+#get
+print(d1.get('Ap'))
+
+#keys
+print(d1.keys())
+
+#values
+print(d1.values())
+
+#for(int i=0;i<10;i++){
+#   logic 
+#}
+#print(i,end='')
+
+#for i in range(1,10,2)
+#  print('odd nos are=',i)
