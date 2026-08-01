a863d37279bfaaa6d33c89806950af4a4c693414
vinaypannangi <vinaypannangi@gmail.com>
2026-08-01 16:23:21 +0530

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,19 @@
+#print the sum of keys in dict
+d4={1:10,2:20,3:30}
+print(d4)
+s=sum(d4.keys())
+print("the sum of keys of dict is:",s)
+
+d1=int(input("enter no"))
+print(d1)
+print(type(d1))
+
+d1=eval(input("enter no"))
+print(d1)
+print(type(d1))
+
+for i,j in d4.items():
+    print("the key is :",i,"the value is:",j,sum is([i,j]))
+
+
+    
