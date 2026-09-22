71af7f14af29b4fa707f8a93ea8951fb9edf481a
vinaypannangi <vinaypannangi@gmail.com>
2026-09-22 16:13:54 +0530

    Add files via upload

---
+++ bar graph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=['A','B','C']
+y=[100,200,300]
+plt.title("line graph analysis")
+plt.xlabel("people")
+plt.ylabel("marks")
+plt.bar(x,y,color="green")
+plt.show()
